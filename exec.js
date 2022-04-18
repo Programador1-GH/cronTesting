c498fb1ae041935d73000e84a7d3b17a1813c665
programador1_PC26 <programador1@grupohereda.com>
2022-04-18 18:19:25 +0200

    Corrigiendo mensaje de salida

--- exec.js
+++ exec.js
@@ -1,8 +1,7 @@
 const child_process = require("child_process");
 const cron = require("node-cron");
 
-console.log("Para ejecución inmediata use: $ node ./exec.js now");
 console.log("cron (* * * * 1-5) esperando...");
 cron.schedule("* * * * 1-5", () => {
-            console.log("-----------------------------");
+            console.log("Esta tarea se ejecuta cada minuto");
         });
